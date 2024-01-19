d6cab15f1a8c2540efbf58ace4fbaab3819719a4
Best25x <xBest25x@gmail.com>
2024-01-18 22:31:42 -0800

    still debugging the csv file

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
+import csv
+
+csvfile = open('submissions.csv', 'w', newline='')
+writer = csv.writer(csvfile)
+writer.writerow(['name', 'email', 'message'])
+submissions = []
+
+for i in submissions:
+    writer.writerow(submissions)
+
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
@@ -29,6 +38,7 @@ def confirmation():
         "email" : email,
         "message" : message
     }
+    submissions.append([name,email,message])
     return render_template("confirmation.html", data=props)
 
 
